043b861fcab23a9f394e8f1521e6eb269e2c7fad
Jorge Perales Diaz <slipknot_jpd@hotmail.com>
2017-02-02 22:34:08 -0600

    First Xamarin Approach

---
+++ EjercicioBase/EnviarCorreo.Nativo/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using EnviarCorreo.Nativo.Services;
+
+namespace EnviarCorreo.Nativo
+{
+    [Activity(Label = "EnviarCorreo.Nativo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button actionButton;
+        string emailBase = "slipknot_jpd@hotmail.com";
+        string codeBase = "upt-Enero28";
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            actionButton = FindViewById<Button>(Resource.Id.btnReportar);
+            actionButton.Click += btnReportar_Click;
+        }
+
+        private async void btnReportar_Click(object sender, EventArgs e)
+        {
+            ServiceHelper serviceHelper = new ServiceHelper();
+            if (emailBase == "amin.espinoza@gmail.com" || codeBase == "iniciativa")
+            {
+                throw new Exception("Recuerda modificar el código fuente para ingresar tu e-mail y ID de evento");
+            }
+            actionButton.Enabled = false;
+            await serviceHelper.InsertarEntidad(emailBase, codeBase);
+            actionButton.Text = "Reporte enviado";
+
+            Toast.MakeText(this, "Tienes correo nuevo!", ToastLength.Short).Show();
+        }
+    }
+}
+
